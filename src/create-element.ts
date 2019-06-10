da37fc27eb8ce836576a4472f4ae8488d21a5764
Jake 'Sid' Smith <jake@dabapps.com>
2019-06-10 20:09:02 +0100

    Update createElement type

--- src/create-element.ts
+++ src/create-element.ts
@@ -1,11 +1,11 @@
 import { flatten } from './internal/utils';
 import { Component, Element } from './types';
 
-export function createElement<P = {}>(
-  type: Component<P>,
+export function createElement<P = {}, S = {}>(
+  type: Component<P, S>,
   props: P,
   ...children: ReadonlyArray<Element>
-): Element<P> {
+): Element<P, S> {
   return {
     type,
     props: {
